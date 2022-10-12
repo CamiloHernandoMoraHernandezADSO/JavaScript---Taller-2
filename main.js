ce2db56cd98ae45a9cd1d7f2bfb61fb6ea68b011
Camilo Mora ADSO <chmora8@misena.edu.co>
2022-10-12 08:27:11 -0500

    Ejercicio 10

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 addEventListener("DOMContentLoaded", (e)=>{
 
     /*
-        Ejercicio 9
-        Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo, 
-        pero cuando se les realiza la entrevista sólo se les pregunta el año en que nacieron. Realice el código 
-        que representen el algoritmo para solucionar este problema.
-        Al final debe entregar el nombre de la persona entrevistada en mayúscula sostenida y minunscula 
-        sostenida y la edad del entrevistado
+        Ejercicio 10
+        Ingrese una frase que lo identifique como programador de Software y luego mostar esta frase 
+        invertida.
     */
     
-        let anio, actual, edad
-        actual = 2022
-        nombre = prompt(`Ingrese el nombre del empleado`)
-        anio = Number(prompt(`Ingrese el año de nacimiento`))
-        edad = actual - anio
+        let frase, invertida 
     
-        alert(`La edad de ${nombre} es ${edad} `)
+        frase = "Programador sin panza, no es de confianza"
     
+        invertida = frase.split("").reverse().join("");
+    
+        alert(invertida)
     })
